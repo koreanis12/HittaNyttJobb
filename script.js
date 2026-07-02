9e0e1e0a9f17e2f7e80849261dabdc775840171f
koreanis12 <karljohansoderdahl@gmail.com>
2026-07-02 16:18:48 +0200

    Change job type for Kock from 'Kock' to 'matlagning'

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
 
 function initiera() {
     allJobb = [
-        {id: 1, titel: "Kock", typ: "Kock", omrade: "Stockholm", beskrivning: "Vi söker pizzabagare", lon: "150 kr/tim", start: "2026-06-15", slut: "2026-08-31", arbetsgivare: "Pizzeria Roma", telefon: "070-123 4567"},
+        {id: 1, titel: "Kock", typ: "matlagning", omrade: "Stockholm", beskrivning: "Vi söker pizzabagare", lon: "150 kr/tim", start: "2026-06-15", slut: "2026-08-31", arbetsgivare: "Pizzeria Roma", telefon: "070-123 4567"},
         {id: 2, titel: "Lagerarbetare", typ: "Lager", omrade: "Södermalm", beskrivning: "Lagerarbete", lon: "140 kr/tim", start: "2026-06-01", slut: "2026-08-31", arbetsgivare: "Logistik Sverige AB", telefon: "070-234 5678"},
         {id: 3, titel: "Städare", typ: "Städ", omrade: "Norrmalm", beskrivning: "Städning", lon: "130 kr/tim", start: "2026-06-15", slut: "2026-08-30", arbetsgivare: "ReNy Städservice", telefon: "070-345 6789"},
         {id: 4, titel: "Kassörsarbetare", typ: "Kassörsarbete", omrade: "Östermalm", beskrivning: "Kassaarbete", lon: "135 kr/tim", start: "2026-07-01", slut: "2026-08-31", arbetsgivare: "ICA Supermarket", telefon: "070-456 7890"}
